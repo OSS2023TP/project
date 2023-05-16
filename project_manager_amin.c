bc53d3b63c6ea44221ca352a7b700f5c0e683965
amin7942 <76994595+amin7942@users.noreply.github.com>
2023-05-16 14:15:52 +0900

    내림차순,오름차순 date함수 구현 but버그있음

--- project_manager_amin.c
+++ project_manager_amin.c
@@ -17,3 +17,43 @@ void show_info(Task *t[], int count){
       t[n]->title, t[n]->due_date, t[n]->Acc, t[n]->compeleted, t[n]->engineer);
     printf("\n%s",t[n]->description);
 }
+int stoi(char *s){
+  if(*s == '\0') return 0;
+  return stoi(s+1) + (*s -'0') * 10;
+}
+
+void ascending_date(Task *t[], int count){
+  Task *ascending[128];
+  for(int i = 0; i < 128; i++){
+    ascending[i] = t[i];
+  }
+  Task *temp;
+  for(int i = 0; i < count; i++){
+    for(int j = i+1; j < count-1; j++){
+      if(stoi(ascending[j]->due_date) < stoi(ascending[i]->due_date)){
+        temp = ascending[i];
+        ascending[i] = ascending[j];
+        ascending[j] = temp;
+      }
+    }
+  }
+  listTask(ascending,count);
+}
+void descending_date(Task *t[], int count){
+  Task *descending[128];
+  for(int i = 0; i < 128; i++){
+    descending[i] = t[i];
+  }
+  Task *temp;
+  for(int i = 0; i < count; i++){
+    for(int j = i+1; j < count-1; j++){
+      if(stoi(descending[j]->due_date) > stoi(descending[i]->due_date)){
+        temp = descending[i];
+        descending[i] = descending[j];
+        descending[j] = temp;
+      }
+    }
+  }
+  listTask(descending,count);
+}
+
